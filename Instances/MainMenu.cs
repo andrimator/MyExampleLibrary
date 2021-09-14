00a8d92ba3f70964195277e089be6d4991db6502
André A <andrimator@gmail.com>
2021-09-14 01:02:55 -0500

    Create MainMenu.cs
    Cimientos de lo que en un futuro será el menú de la librería en Consola.

---
+++ Instances/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExampleLibrary
+{
+    class MainMenu
+    {
+        public void DrawGUI()
+        {
+            //Este metodo dibujara el menu
+        }
+
+        public int GetInput()
+        {
+            int input = 0;
+            Console.Write("Input: ");
+            while (input == 0)
+            {
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (SystemException)
+                {
+
+                }
+            }
+            return input;
+        }
+    }
+}
